bc75713e6631b6b19ae89444036b366e9e3b3947
Alex Truong <harkongunlion@msn.com>
2014-02-01 12:28:33 -0500

    adding Adeel's pages
    blend.htm is the individual blend page, while blend_view.htm is the
    blend view page. I altered the original names slightly for comprehension
    reasons.

---
+++ angular-joe/js/data.js
@@ -0,0 +1,92 @@
+var desc = {
+    "After Dinner":{
+        "description": "This smooth and silky roast has the rich tones of darkly roasted Indonesian, Ethiopian and Guatemalan coffee beans. You'll find this full-bodied blend to be a great way to finish a meal and ease you into the rest of your night.",
+        "regions": ["Indonesian", "Ethiopian", "Guatemalan"],
+        "tags": ["#bold"]
+    },
+    "Bourque Newswatch":{
+        "description": "Get your morning started with this bright and peppy light-bodied blend. This sweet and nutty aroma will wake you up and keep you up throughout the day. Enjoy the great fruit and acidity of these Blue Mountain, Ethiopian and Kenyan beans.",
+        "regions": ["Blue Mountain", "Kenyan", "Ethiopian"],
+        "tags": ["#light"]
+    },
+    "CN Tower of Power":{
+        "description": "Feel on top of the world with this well-balanced, chocolately, full-bodied nectar of the gods. Our Ethiopian and Brazilian beans pack a power punch, eh.",
+        "regions": ["Ethiopian", "Brazilian"],
+        "tags": ["#bold"]
+    },
+    "Connoisseur Estates":{
+        "description": "Rich and palate-pleasing, this amazing combination of Ethiopian, Kona and Brazilian beans pair perfectly with our Jittery Joe's acai berry scones. You'll enjoy the subtle hints of tobacco, barnyard, earth, and watermelon.",
+        "regions": ["Ethiopian", "Kona", "Brazilian"],
+        "tags": ["#medium"]
+    },
+    "Columbian":{
+        "description": "This traditional single-varietal blend will soothe all your senses. It has a fruity and earthy undertone and a snappy finish. Try this amazing fair-trade version.",
+        "regions": ["Colombian"],
+        "tags": ["#medium"]
+    },
+    "Dutch Trader":{
+        "description": "Rich and chocolately, the hints of anise will bring you back to your last trip in the Netherlands. You'll want to shuffle around in your wooden clogs once you get a taste of this Indonesian, Ethiopian, and Costa Rican blend.",
+        "regions": ["Indonesian", "Ethiopian", "Costa Rican"],
+        "tags": ["#light"]
+    },
+    "Fordnation Blend":{
+        "description": "This blend of Blue Mountain, Guatemalan, and Colombian beans will surprise you at every moment. You'll always wish you had enough of this to drink at home.",
+        "regions": ["Blue Mountain", "Guatemalan", "Colombian"],
+        "tags": ["#bold"]
+    },
+    "French Roast":{
+        "description": "Get your croissant ready for this smooth and bright roast. This blend of Brazilian, Cuban, Dominican, and Haitian beans are perfect for a stroll down the Champs Elysees. C'est fantastique!",
+        "regions": ["Brazilian", "Cuban", "Dominican", "Haitian"],
+        "tags": ["#medium"]
+    },
+    "Hawaii Kona":{
+        "description": "Aloha! Throw on your grass skirt and grab your coconuts, this single-origin coffee will get you up and smelling the ocean air. Bright and beautiful Kona beans straight from the islands. Aloha!",
+        "regions": ["Kona"],
+        "tags": ["#bold"]
+    },
+    "House Blend":{
+        "description": "No description available.",
+        "regions": ["Bolivia", "Rwanda", "El Salvador"],
+        "tags": []
+    },
+    "Italian Roast":{
+        "description": "No description available.",
+        "regions": ["Ethiopian", "Kenyan", "Ghana"],
+        "tags": []
+    },
+    "Lionel Roastie":{
+        "description": "No description available.",
+        "regions": ["Kona", "Indonesian", "Kenyan"],
+        "tags": []
+    },
+    "Mocca-Java":{
+        "description": "No description available.",
+        "regions": ["Colombian", "Costa Rican", "Mexico"],
+        "tags": []
+    },
+    "Reggae Blend":{
+        "description": "No description available.",
+        "regions": ["Blue Mountain", "Cuban", "El Salvador"],
+        "tags": []
+    },
+    "Ruth Roast":{
+        "description": "No description available.",
+        "regions": ["Philippines", "Indonesia", "Vietnam"],
+        "tags": []
+    },
+    "Toronto Blend":{
+        "description": "No description available.",
+        "regions": ["Brazilian", "Colombian", "India A", "Vietnamese"],
+        "tags": []
+    },
+    "Tropic of Coffee":{
+        "description": "No description available.",
+        "regions": ["India A", "Mexico", "Kona"],
+        "tags": []
+    },
+    "World Tour Blend":{
+        "description": "No description available.",
+        "regions": ["EVERY BEAN (wat)"],
+        "tags": []
+    }
+};
